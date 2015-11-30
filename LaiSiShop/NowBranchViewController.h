28b95208ca150708550899a4e36a506338a9b0d6
easyhaipi <easy_qingsong@163.com>
2015-11-30 14:03:32 +0800

    测试当前在那个分支

---
+++ LaiSiShop/NowBranchViewController.h
@@ -0,0 +1,13 @@
+//
+//  NowBranchViewController.h
+//  LaiSiShop
+//
+//  Created by taobaichi on 15/11/30.
+//  Copyright © 2015年 taobaichi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NowBranchViewController : UIViewController
+
+@end
---
+++ LaiSiShop/NowBranchViewController.m
@@ -0,0 +1,37 @@
+//
+//  NowBranchViewController.m
+//  LaiSiShop
+//
+//  Created by taobaichi on 15/11/30.
+//  Copyright © 2015年 taobaichi. All rights reserved.
+//  测试当前在那个分支
+
+#import "NowBranchViewController.h"
+
+@interface NowBranchViewController ()
+
+@end
+
+@implementation NowBranchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
